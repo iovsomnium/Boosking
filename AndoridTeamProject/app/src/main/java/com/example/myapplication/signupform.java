b8e82bd63d66c4eb1bc2462ae1700030bb123087
iovsomnium <ksw09157.5212@gmail.com>
2019-12-03 16:45:35 +0900

    add signupform

--- AndoridTeamProject/app/src/main/java/com/example/myapplication/signupform.java
+++ AndoridTeamProject/app/src/main/java/com/example/myapplication/signupform.java
@@ -2,12 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class signupform extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,9 +12,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void goToSignUpForm(View view)
-    {
-        Intent intent = new Intent(this, signupform.class);
-        startActivity(intent);
-    }
+
 }
