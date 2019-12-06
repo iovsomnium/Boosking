92b67429d6229959cd16de6059f097b45fb0d6a4
Choi-YeonJin <41119288+Choi-YeonJin@users.noreply.github.com>
2019-12-07 04:22:45 +0900

    채팅 db에 insert와 뿌려주기 구현

--- CheTiBleEX/app/src/main/java/com/example/che_ti_bleEX/ChatActivity.java
+++ CheTiBleEX/app/src/main/java/com/example/che_ti_bleEX/ChatActivity.java
@@ -8,14 +8,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Hashtable;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -24,10 +31,23 @@ public class ChatActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
 
+    String email;
+    EditText chat_txt;
+    Button btn_add;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            email = user.getEmail();
+
+        }
+
+        chat_txt = (EditText)findViewById(R.id.chat_txt);
+        btn_add = (Button)findViewById(R.id.btn_add);
 
         recyclerView = (RecyclerView)findViewById(R.id.rv);
         linearLayoutManager = new LinearLayoutManager(this);
@@ -37,14 +57,29 @@ public class ChatActivity extends AppCompatActivity {
 
         mainAdapter = new MainAdapter(arrayList);
         recyclerView.setAdapter(mainAdapter);
-
-        Button btn_add = (Button) findViewById(R.id.btn_add);
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MainData mainData = new MainData(R.mipmap.ic_launcher,"최연진","리사이클러뷰");
-                arrayList.add(mainData);
-                mainAdapter.notifyDataSetChanged();
+
+                String stText = chat_txt.getText().toString();
+                if(stText.equals("") || stText.isEmpty()){
+                    Toast.makeText(ChatActivity.this,"내용을 입력해 주세요", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(ChatActivity.this,email+","+stText, Toast.LENGTH_SHORT).show();
+
+                    Calendar c = Calendar.getInstance();
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String formattedData = df.format(c.getTime());
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("chats").child(formattedData);
+
+                    MainData mainData = new MainData(R.mipmap.ic_launcher,email,stText);
+                    arrayList.add(mainData);
+                    myRef.setValue(mainData);
+                    mainAdapter.notifyDataSetChanged();
+                }
+
             }
         });
     }
