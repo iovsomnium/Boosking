399b8ff8ed020e04a6a96dbfccd261c651a7f4db
iovsomnium <ksw09157.5212@gmail.com>
2019-12-09 23:42:51 +0900

    add design user and they list

---
+++ CheTiBleEX/app/src/main/java/com/example/che_ti_bleEX/User.java
@@ -0,0 +1,26 @@
+package com.example.che_ti_bleEX;
+
+public class User {
+    private String name;
+    private String subject;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public  User(){
+
+    }
+}
