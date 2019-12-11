8e9988fafea790e56b51fe0598b21a234b993faa
Choi-YeonJin <41119288+Choi-YeonJin@users.noreply.github.com>
2019-12-11 11:53:58 +0900

    이름 띄우기

--- CheTiBleEX/app/src/main/java/com/example/che_ti_bleEX/ChangeTimetableActivity.java
+++ CheTiBleEX/app/src/main/java/com/example/che_ti_bleEX/ChangeTimetableActivity.java
@@ -1,14 +1,80 @@
 package com.example.che_ti_bleEX;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class ChangeTimetableActivity extends AppCompatActivity {
+
+    InputMethodManager imm;
+    Spinner date,period;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_timetable);
+
+        imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+
+        date = (Spinner)findViewById(R.id.date);
+        period = (Spinner)findViewById(R.id.period);
+
+        final ArrayList datearray = new ArrayList<>();
+        datearray.add("월요일");
+        datearray.add("화요일");
+        datearray.add("수요일");
+        datearray.add("목요일");
+        datearray.add("금요일");
+
+        ArrayList periodarray = new ArrayList<>();
+        periodarray.add("1교시");
+        periodarray.add("2교시");
+        periodarray.add("3교시");
+        periodarray.add("4교시");
+        periodarray.add("5교시");
+        periodarray.add("6교시");
+        periodarray.add("7교시");
+
+        ArrayAdapter dateAdapter = new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,datearray);
+        ArrayAdapter periodAdapter = new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,periodarray);
+
+        date.setAdapter(dateAdapter);
+        period.setAdapter(periodAdapter);
+
+        date.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                imm.hideSoftInputFromWindow(date.getWindowToken(), 0);
+                        Toast.makeText(getApplicationContext(),datearray.get(i)+"가 선택되었습니다.",
+                                Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        period.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                imm.hideSoftInputFromWindow(period.getWindowToken(), 0);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
     }
 }
--- CheTiBleEX/app/src/main/java/com/example/che_ti_bleEX/HomeActivity.java
+++ CheTiBleEX/app/src/main/java/com/example/che_ti_bleEX/HomeActivity.java
@@ -17,8 +17,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -81,6 +85,30 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent set = new Intent(getApplicationContext(),SettingActivity.class);
                 startActivity(set);
+            }
+        });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        mPostReference = firebaseDatabase.getReference("Teacher");
+
+        teachername = (TextView)findViewById(R.id.username);
+
+        Query query = mPostReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    String name = ""+ ds.child("name").getValue();
+
+                    teachername.setText(name);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
