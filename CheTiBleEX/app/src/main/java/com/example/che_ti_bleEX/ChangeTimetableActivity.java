3eea2207dcd86fe894a2e6504ac11033ccce0024
Choi-YeonJin <41119288+Choi-YeonJin@users.noreply.github.com>
2019-12-11 10:06:32 +0900

    ㅁㄴㅇㅎ

---
+++ CheTiBleEX/app/src/main/java/com/example/che_ti_bleEX/ChangeTimetableActivity.java
@@ -0,0 +1,14 @@
+package com.example.che_ti_bleEX;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangeTimetableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_timetable);
+    }
+}
